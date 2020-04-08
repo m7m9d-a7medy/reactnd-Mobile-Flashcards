1f5bae81ae4900e288c61accbd5ae52dfddcedfd
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-08 21:02:29 +0200

    feat: Reusable TextButton

---
+++ components/UI/TextButton.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+const TextButton = ({ children, onPress }) => {
+    return (
+        <TouchableOpacity
+            style={[styles.button]}
+            onPress={onPress}
+        >
+            <Text style={[styles.text]}>{children}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        margin: 10,
+        width: '50%',
+        alignContent: 'center',
+        justifyContent: 'center',
+        paddingVertical: 25,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#000',
+        fontSize: 25,
+    },
+    text: {
+        textAlign: 'center',
+        color: '#000',
+        fontSize: 22,
+    }
+})
+
+export default TextButton

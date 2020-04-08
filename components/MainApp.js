ce45c6a74e0d78961786e2808feffa6b82c74c09
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-08 12:15:58 +0200

    feat: Main app redux binding

--- components/MainApp.js
+++ components/MainApp.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import DecksTab from './tabs/Decks/DecksTab'
 import NewDeckTab from './tabs/NewDeck/NewDeckTab'
+// Test
+import { saveDecks } from '../store/actions'
+import { saveMockData } from '../utils/asyncStorage'
 
 const Tabs = createMaterialTopTabNavigator()
 
 export class MainApp extends Component {
+  componentDidMount() {
+    saveMockData()
+      .then(() => {
+        this.props.dispatch(saveDecks())
+      })
+  }
+
   render() {
     return (
       <Tabs.Navigator>
-        <Tabs.Screen name='Decks' component={DecksTab}/>
-        <Tabs.Screen name='New Deck' component={NewDeckTab}/>
+        <Tabs.Screen name='Decks' component={DecksTab} />
+        <Tabs.Screen name='New Deck' component={NewDeckTab} />
       </Tabs.Navigator>
     )
   }
 }
 
-export default MainApp
+export default connect()(MainApp)

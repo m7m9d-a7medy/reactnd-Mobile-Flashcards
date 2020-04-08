27f55b3a4ed1a40f1f57290aeb80df02188b35d5
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-04-08 12:07:37 +0200

    refactor: Consistent data

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -1,12 +1,17 @@
+import { generateId } from "./helpers";
+
 export default {
     React: {
         title: 'React',
+        id: generateId(),
         questions: [
             {
+                id: generateId(),
                 question: 'What is React?',
                 answer: 'A library for managing user interfaces'
             },
             {
+                id: generateId(),
                 question: 'Where do you make Ajax requests in React?',
                 answer: 'The componentDidMount lifecycle event'
             }
@@ -14,8 +19,10 @@ export default {
     },
     JavaScript: {
         title: 'JavaScript',
+        id: generateId(),
         questions: [
             {
+                id: generateId(),
                 question: 'What is a closure?',
                 answer: 'The combination of a function and the lexical environment within which that function was declared.'
             }
